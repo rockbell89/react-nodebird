942968de87917b6e9fd15bd4f0dedaff5cb34f34
ria-nanuda <kbr@widaehan.com>
2022-01-05 15:05:31 +0900

    edit text

--- front/pages/signup/index.js
+++ front/pages/signup/index.js
@@ -55,7 +55,7 @@ const Signup = () => {
   return (
     <>
       <Head>
-        <title>회원가입 | Nodebird</title>
+        <title>회원가입</title>
       </Head>
       <Card>
         <Form onFinish={onSubmit}>
